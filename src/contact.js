17f76048a13735e6664b98983da5102f84d57a86
Mohammad Alwazzan <kingwazzan2@gmail.com>
2022-08-20 07:13:05 +0300

    Update contact.js

--- src/contact.js
+++ src/contact.js
@@ -1,17 +1,17 @@
-export function createConatct(container){
+export function createConatct(container) {
     const contactConatiner = document.createElement('div')
     const phoneContainer = document.createElement('div')
     const phoneIcon = document.createElement('div')
     const phoneNumber = document.createElement('div')
     const email = document.createElement('div')
-    
+
     contactConatiner.classList.add('contactContainer')
     phoneContainer.classList.add('phoneContainer')
     phoneIcon.classList.add('phoneIcon')
     phoneNumber.classList.add('phoneNumber')
     email.classList.add('Email')
-    
-    
+
+
     phoneIcon.innerHTML = '<img class ="icon" src="pics/call.png">'
     phoneNumber.textContent = '123-456-789'
     email.textContent = 'Email: totallyReal@gmail.com'
@@ -21,5 +21,5 @@ export function createConatct(container){
     phoneContainer.appendChild(phoneIcon)
     phoneContainer.appendChild(phoneNumber)
 
-    
+
 }
